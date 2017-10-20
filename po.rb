e15c43b08e25d8581bcb1904df5bda7e418749cc
Nathan Robinson <nrobinson2000@me.com>
2017-10-20 15:05:23 -0400

    at 15:05 of 2017-10-20

---
+++ po.rb
@@ -0,0 +1,39 @@
+#                                            __      __  __
+#                                           /  |    /  |/  |
+#     ______    ______           __    __  _██ |_   ██/ ██ |
+#    /      \  /      \  ______ /  |  /  |/ ██   |  /  |██ |
+#   /██████  |/██████  |/      |██ |  ██ |██████/   ██ |██ |
+#   ██ |  ██ |██ |  ██ |██████/ ██ |  ██ |  ██ | __ ██ |██ |
+#   ██ |__██ |██ \__██ |        ██ \__██ |  ██ |/  |██ |██ |
+#   ██    ██/ ██    ██/         ██    ██/   ██  ██/ ██ |██ |
+#   ███████/   ██████/           ██████/     ████/  ██/ ██/
+#   ██ |
+#   ██ |
+#   ██/                  https://po-util.com
+#
+
+#  po-util - The Ultimate Local Particle Experience for Linux and macOS
+# Copyright (C) 2017  Nathan Robinson
+
+class Po < Formula
+  desc "Particle Offline Utility: The Ultimate Local Particle Experience"
+  homepage 'https://po-util.com'
+  head 'https://github.com/nrobinson2000/homebrew-po', :using => :git
+  url 'https://github.com/nrobinson2000/homebrew-po/archive/135.tar.gz'
+  sha256 '25b1758d9686633cb57fd9d8c5fd7a54df52aa1a3fb11ea0995566ac6b798a9d'
+
+  def install
+    bin.install 'po'
+    man1.install 'man/po.1'
+    bash_completion.install "completion/po"
+  end
+
+  def caveats; <<-EOS.undent
+
+  In order to complete the installation of po-util,
+  you must run 'po install'.
+
+  For Raspberry Pi support you must have docker installed.
+  EOS
+  end
+end
